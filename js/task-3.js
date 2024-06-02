68c4b46ce55f5c499a2f817a5fe89b041c5fcd42
Pavlo <95445682+pkondratiuk@users.noreply.github.com>
2024-06-02 21:21:33 +0300

    hw-06-01
    hw finish

---
+++ js/task-3.js
@@ -0,0 +1,30 @@
+class StringBuilder {
+    #value;
+
+    constructor(value){
+        this.#value = value;
+    }
+   
+    getValue(){
+        return this.#value;
+    }
+    padEnd(str){
+        this.#value = str + this.#value;
+    }
+      padStart(str){
+        this.#value = this.#value + str;
+      }
+
+      padBoth(str){
+        this.#value = str + this.#value + str;
+      }
+};
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
